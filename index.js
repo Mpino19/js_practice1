1e20a01f710adadfdd7fc64469dd8b79dd491922
Mayu Izumi <mayu@MayunoMacBook-Air.local>
2024-05-19 16:34:42 +0900

    課題2

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ let number2 = 5;
 console.log(number1*number2);
 number1 = 7;
 console.log(number1*number2);
+
+let point = 70
+
+if (point >= 80) {
+  console.log("素晴らしい");
+} else if (point < 80 && point >= 60) {
+  console.log("合格です");
+} else {
+  console.log("不合格です");
+}
