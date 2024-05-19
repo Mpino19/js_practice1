7b4754a803938c825a88321a8fa516c51194edc0
Mayu Izumi <mayu@MayunoMacBook-Air.local>
2024-05-19 17:29:22 +0900

    課題3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log(number1*number2);
 number1 = 7;
 console.log(number1*number2);
 
+
 let point = 70
 
 if (point >= 80) {
@@ -13,3 +14,18 @@ if (point >= 80) {
 } else {
   console.log("不合格です");
 }
+
+
+class User {
+  constructor (id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  sayName () {
+    console.log(`私の名前は${this.firstName}${this.lastName}です。`);
+  }
+}
+const u1 = new User (1, "太郎", "鈴木", 25);
+u1.sayName();
